0547d03479b0c88027af485eb626aad2f2b0bdc9
Tom Brown <nottombrown@gmail.com>
2016-04-18 12:03:35 -0700

    Basic loss function

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -19,17 +19,47 @@ def softmax_loss_naive(W, X, y, reg):
   - loss as single float
   - gradient with respect to weights W; an array of same shape as W
   """
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
 
   #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+  # Compute the softmax loss and its gradient using explicit loops.     #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+
+  for i in xrange(num_train):
+    x = X[i]
+    scores = x.dot(W)
+    correct_class = y[i]
+
+    exp_scores = np.exp(scores)
+    normalized_exp_scores = exp_scores / np.sum(exp_scores)
+
+    correct_class_score = normalized_exp_scores[correct_class]
+
+    loss += - np.log(correct_class_score)
+
+    # Update the gradient for the weights that feed the correct class
+
+
+  # Right now the loss and dW are sums over all training examples, but we
+  # want them to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss and the gradient.
+  loss += 0.5 * reg * np.sum(np.square(W))
+
+  # For each weight Wij, the partial derivative dWij of the loss function above is:
+  #     0.5 * reg * 2.0 * Wij
+  dW += reg * W
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -59,4 +89,3 @@ def softmax_loss_vectorized(W, X, y, reg):
   #############################################################################
 
   return loss, dW
-
---
+++ assignment1/cs231n/tests/softmax_test.py
@@ -0,0 +1,106 @@
+
+import numpy as np
+import pytest
+from numpy.testing import assert_allclose
+
+from cs231n.classifiers import softmax_loss_naive, softmax_loss_vectorized
+"""
+ N = 3
+ C = 2
+ D = 4
+"""
+
+@pytest.fixture
+def X():
+  """
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  """
+  return np.array(
+    [
+      [1, 2, 2, 1],
+      [4, 3, 4, 4],
+      [3, 4, 4, 2],
+    ])
+
+@pytest.fixture
+def y():
+  """
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  """
+  return np.array(
+    [
+      0,
+      1,
+      1,
+    ])
+
+@pytest.fixture
+def W_zeros():
+  """
+  - W: A numpy array of shape (D, C) containing weights.
+  """
+  return np.zeros((4, 2))
+
+@pytest.fixture
+def W_ones():
+  """
+  - W: A numpy array of shape (D, C) containing weights.
+  """
+  return np.zeros((4, 2)) + 1
+
+
+def test_softmax_loss_naive(W_zeros, X, y):
+  loss, gradient = softmax_loss_naive(W_zeros, X, y, reg=1.0)
+  assert loss == 0.69314718055994529
+
+  desired_gradient = np.array([
+    [2., -2.],
+    [1.666667, -1.666667],
+    [2., -2.],
+    [1.666667, -1.666667]
+  ])
+
+  # assert_allclose(desired_gradient, gradient, 0.00001)
+#
+# def test_softmax_loss_naive_with_weights(W_ones, X, y):
+#   loss, gradient = softmax_loss_naive(W_ones, X, y, reg=1.0)
+#   assert loss == 5.0
+#
+#   desired_gradient = np.array([
+#     [3., -1.],
+#     [2.666667, -0.666667],
+#     [3., -1.],
+#     [2.666667, -0.666667]])
+#
+#
+#   assert_allclose(desired_gradient, gradient, 0.00001)
+#
+#
+# def test_softmax_loss_vectorized(W_zeros, X, y):
+#   loss, gradient = softmax_loss_vectorized(W_zeros, X, y, reg=1.0)
+#   assert loss == 1
+#
+#   desired_gradient = np.array([
+#     [2., -2.],
+#     [1.666667, -1.666667],
+#     [2., -2.],
+#     [1.666667, -1.666667]
+#   ])
+#
+#   assert_allclose(desired_gradient, gradient, 0.00001)
+#
+#
+#
+# def test_softmax_loss_vectorized_with_weights(W_ones, X, y):
+#   loss, gradient = softmax_loss_vectorized(W_ones, X, y, reg=1.0)
+#   assert loss == 5.0
+#
+#   desired_gradient = np.array([
+#     [3., -1.],
+#     [2.666667, -0.666667],
+#     [3., -1.],
+#     [2.666667, -0.666667]])
+#
+#
+#   assert_allclose(desired_gradient, gradient, 0.00001)
